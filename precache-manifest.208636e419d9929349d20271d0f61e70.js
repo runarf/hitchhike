65918b52dc890f3efe4a981aaa8dff1220111be2
Runar <runar@tuta.io>
2019-12-20 14:43:08 +0100

    Updates

---
+++ precache-manifest.208636e419d9929349d20271d0f61e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa0e2d54a5573625ebbe6d01b06bab0",
+    "url": "/hitchhike/index.html"
+  },
+  {
+    "revision": "6a3e2918a380c1fdc089",
+    "url": "/hitchhike/static/css/2.61091e02.chunk.css"
+  },
+  {
+    "revision": "6a3e2918a380c1fdc089",
+    "url": "/hitchhike/static/js/2.4cd60aa5.chunk.js"
+  },
+  {
+    "revision": "bdc7727bc0f16ac349efac0552ddc4e5",
+    "url": "/hitchhike/static/js/2.4cd60aa5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f74e7ea43f05965ee394",
+    "url": "/hitchhike/static/js/main.1c1144c0.chunk.js"
+  },
+  {
+    "revision": "1fd8a690cf26f255929d",
+    "url": "/hitchhike/static/js/runtime-main.2e072454.js"
+  }
+]);
